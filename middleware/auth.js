55addce716f0a733cdb260dfafc7ef866f338fa1
hs <sibbengaharry@gmail.com>
2021-11-02 13:10:21 +0000

    Added middleware for route gaurds

---
+++ middleware/auth.js
@@ -0,0 +1,13 @@
+export default function ({ store, redirect, route }) {
+  const user = store.state.users.user
+  const blockedRoute = /\/dashboard\/*/g
+  const loginRoute = '/login'
+
+  if (!user && route.path.match(blockedRoute)) {
+    redirect('/login')
+  }
+
+  if (user && route.path === loginRoute) {
+    redirect('/admin')
+  }
+}
